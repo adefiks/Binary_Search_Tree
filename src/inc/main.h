e77263b1608a3a1dfac30b0c726827876fa87e90
Piotr Potocki <p.potocki@is-wireless.com>
2020-05-14 23:24:39 +0200

    [main] added input

--- src/inc/main.h
+++ src/inc/main.h
@@ -4,3 +4,10 @@ void test()
 {
     std::cout << "test" << std::endl;
 }
+
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
+#include <string.h>
+#include <math.h>
 #include "inc/main.h"
 using namespace std;
 
-int main(int argc, char *[])
+int main(const int argc, char *args[])
 {
-    cout << "Hej Huj" << endl;
-    cout << "Hej Huj" << endl;
-    cout << "Hej Huj" << endl;
-    cout << "Hej Huj" << endl;
-    cout << "Hej Huj" << endl;
+    cout << "Start ";
+    for (size_t i = 2; i < strlen(args[0]); i++)
+        cout << args[0][i];
+    cout << " program" << endl;
+
+    node nodes[argc - 1];
+
+    for (int i = 1; i < argc; i++)
+    {
+        nodes[i].data = 0;
+        for (size_t j = 0; j < strlen(args[i]) - 1; j++)
+        {
+            nodes[i].data += (static_cast<int>(args[i][j]) - 48) * pow(10, (((strlen(args[i]) - 1) - j)));
+        }
+        nodes[i].data += static_cast<int>(args[i][strlen(args[i]) - 1]) - 48;
+        cout << "node[" << i << "]:" << nodes[i].data << endl;
+    }
+
     test();
 
     return 0;
