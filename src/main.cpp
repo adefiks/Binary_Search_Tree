e77afbc416721b257af783297698a9e0078f3277
Piotr Potocki <p.potocki@is-wireless.com>
2020-05-14 10:04:20 +0200

    first

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "inc/main.h"
+using namespace std;
+
+int main(int argc, char *[])
+{
+    cout << "Hej Huj" << endl;
+    cout << "Hej Huj" << endl;
+    cout << "Hej Huj" << endl;
+    cout << "Hej Huj" << endl;
+    cout << "Hej Huj" << endl;
+    test();
+
+    return 0;
+}
